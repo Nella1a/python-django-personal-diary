d40c4bc9877c91480bb80b003b947b3f7f9b6973
Nella1a <m.allen@gmx.at>
2022-06-20 21:53:24 +0200

    Connect urls for the entries app to the urlpatterns list of diary

--- entries/urls.py
+++ entries/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 
-from . import views
+from .views import EntryDetailView, EntryListView
 
 urlpatterns = [
     path(
         "",
-        views.EntryListView.as_view(),
+        EntryListView.as_view(),
         name="entry-list"
     ),
     path(
         "entry/<int:pk>",
-        views.EntryDetailView.as_view(),
+        EntryDetailView.as_view(),
         name="entry-detail"
     ),
 ]
